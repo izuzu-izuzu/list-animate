967f0f6df5fc403c14c956b89333a0ac965ab561
izuzu <izuzu@izuzu.local>
2021-10-28 05:05:44 +0800

    feat: Integrate interpreter and `(++)` page

--- src/Interactive/TUI/Main.hs
+++ src/Interactive/TUI/Main.hs
@@ -15,7 +15,7 @@
 -}
 module Interactive.TUI.Main (main) where
 
-import Control.Lens (makeLenses, (^.), (.~))
+import Control.Lens (makeLenses, (^.), (.~), (%~))
 import Data.Text (Text)
 
 import Brick
@@ -31,43 +31,10 @@ import Data.Maybe (fromMaybe)
 import Data.List (intersperse)
 import Brick.Widgets.Center (hCenter)
 import Brick.Widgets.Border (border)
-
-data Input = Input
-    { _mode1Edit :: Text
-    , _mode2Edit1 :: Text
-    , _mode2Edit2 :: Text
-    , _mode3Edit :: Text
-    , _navChoice :: NavChoice
-    }
-    deriving Show
-
-data Name
-    = Mode1EditField
-    | Mode2Edit1Field
-    | Mode2Edit2Field
-    | Mode3EditField
-    | NavChoiceField
-    | NavCurrField
-    | NavHomeField
-    | NavPrevField
-    | NavNextField
-    | NavQuitField
-    deriving (Bounded, Enum, Eq, Ord, Show)
-
-data NavChoice = NavCurr | NavHome | NavPrev | NavNext | NavQuit
-    deriving (Bounded, Enum, Eq, Show)
-
-data Mode = Mode1 | Mode2 | Mode3
-    deriving (Bounded, Enum, Eq, Show)
-
-data State e = State
-    { _mode :: Mode
-    , _form :: Form Input e Name
-    , _prompt :: String
-    }
-
-makeLenses ''Input
-makeLenses ''State
+import Interactive.TUI.Core
+import Interactive.TUI.Interpreter
+import Interactive.TUI.Append
+import qualified Interactive.TUI.Append as Append
 
 main :: IO ()
 main = do
@@ -88,66 +55,41 @@ main = do
 
     putStrLn "App ends here."
 
-makeNavField :: Text -> Input -> FormFieldState Input e Name
-makeNavField currentLabel =
-    setFieldConcat (\(x:xs) -> x <+> fill ' ' <+> hBox xs)
-    . radioCustomField
-        ' '
-        ' '
-        ' '
-        navChoice
-        (zip3
-            [NavCurr .. NavQuit]
-            [NavCurrField .. NavQuitField]
-            (fmap
-                (<> "    ")
-                [ "*" <> currentLabel <> "*"
-                , "[Home]"
-                , "[Previous]"
-                , "[Next]"
-                , "[Quit]"
-                ]))
-       
-makeForm :: Mode -> Input -> Form Input e Name
-makeForm Mode1 = newForm
-    [ editTextField mode1Edit Mode1EditField (Just 3)
-    , makeNavField "Mode1"
-    ]
-makeForm Mode2 = newForm
-    [ editTextField mode2Edit1 Mode2Edit1Field (Just 2)
-    , editTextField mode2Edit2 Mode2Edit2Field (Just 1)
-    , makeNavField "Mode2"
-    ]
-makeForm Mode3 = newForm
-    [ editTextField mode3Edit Mode3EditField (Just 3)
-    , makeNavField "Mode3"
-    ]
-
-makePrompt :: Mode -> Input -> String
-makePrompt _ _ = "<prompt>"
-
 drawUI :: State e -> [Widget Name]
-drawUI s = [renderForm f <=> str p]
+drawUI s = [renderForm f <=> str o]
     where
         m = s ^. mode
         f = s ^. form
-        p = s ^. prompt
+        o= s ^. output
 
+makeModeForm :: Mode -> Input -> Form Input e Name
+makeModeForm Home = newForm
+    [ radioField
+        emptyInputField
+        [ ((), SelectFnAppendField, "(++) :: [a] -> [a] -> [a]")
+        ]
+    ]
+makeModeForm FnAppend = Append.makeForm
+makeModeForm _ = newForm []
+
+modePreviewEvent :: Mode -> State e -> EventM Name (Next (State e))
+modePreviewEvent Home = continue . (output .~ "<preview>")
+modePreviewEvent FnAppend = Append.previewEvent
+modePreviewEvent _ = continue
+
+modeAnimateEvent :: Mode -> State e -> EventM Name (Next (State e))
+modeAnimateEvent FnAppend = Append.animateEvent
+modeAnimateEvent _ = continue . (output .~ "<animate>")
+
+{-
 syncToMode :: State e -> State e
 syncToMode s@State{_mode = m, _form = f} = s
     { _form = makeForm m $ formState f
-    , _prompt = makePrompt m $ formState f
+    , _output = makePrompt m $ formState f
     }
+-}
 
-navByFrom :: NavChoice -> Mode -> Mode
-navByFrom NavCurr m = m
-navByFrom NavPrev Mode1 = Mode3
-navByFrom NavPrev m = pred m
-navByFrom NavNext Mode3 = Mode1
-navByFrom NavNext m = succ m
-navByFrom NavHome _ = Mode1
-navByFrom NavQuit m = m
-
+{-
 navigateMode :: NavChoice -> State e -> State e
 navigateMode nav s@State{_mode = m, _form = f} = s
     { _mode = m'
@@ -155,22 +97,24 @@ navigateMode nav s@State{_mode = m, _form = f} = s
     , _prompt = makePrompt m' $ formState f
     }
     where m' = navByFrom nav m
-
+-}
+{-
 updateMode :: State e -> State e
-updateMode s@State{_mode = m, _form = f, _prompt = p} = s
+updateMode s@State{_mode = m, _form = f, _output = o} = s
     { _mode = m'
     , _form = f'
-    , _prompt = p'
+    , _output = o'
     }
     where
         nav = (^. navChoice) $ formState f
         m' = navByFrom nav m
         f' = case nav of
-            NavCurr -> f
-            _ -> makeForm m' $ formState f
-        p' = case nav of
-            NavCurr -> p
-            _ -> makePrompt m' $ formState f
+            NavCurrent -> f
+            _ -> f
+        o' = case nav of
+            NavCurrent -> o
+            _ -> o
+-}
 
 themeMap :: AttrMap
 themeMap = attrMap defAttr
@@ -182,35 +126,38 @@ themeMap = attrMap defAttr
     , (listSelectedAttr, white `on` green)
     ]
 
-appEvent
-    :: State e
-    -> BrickEvent Name e
-    -> EventM Name (Next (State e))
-appEvent state (VtyEvent EvResize{}) = continue state
-appEvent state (VtyEvent (EvKey KEsc [])) = halt state
-appEvent state (VtyEvent (EvKey KEnter [])) = do
+appEvent :: State e -> BrickEvent Name e -> EventM Name (Next (State e))
+appEvent state ResizeEvent = continue state
+appEvent state (KEscEvent []) = halt state
+appEvent state (KEnterEvent []) = do
     let
-        focus = 
-            fromMaybe NavCurrField
+        focus =
+            fromMaybe NavCurrentField
             . focusGetCurrent
             . formFocus
             . (^. form)
             $ state
-    state' <-
-        if focus `elem` [NavCurrField .. NavQuitField]
-        then updateMode <$> handleEventLensed
-             state
-             form
-             handleFormEvent
-             (VtyEvent (EvKey (KChar ' ') []))
-        else pure $ updateMode state
-    let form' = setFormFocus focus $ state' ^. form
     case focus of
-        NavQuitField -> halt state'
-        _ -> continue . (form .~ form') $ state'
+        NavCurrentField -> continue state
+        NavHomeField ->
+            continue
+            . (output .~ "<prompt>")
+            . (form .~ (makeModeForm Home . formState . (^. form) $ state))
+            . (mode .~ Home)
+            $ state
+        NavQuitField -> halt state
+        NavPreviewField -> modePreviewEvent (state ^. mode) state
+        NavAnimateField -> modeAnimateEvent (state ^. mode) state
+        SelectFnAppendField ->
+            continue
+            . (output .~ "<prompt>")
+            . (form .~ (makeModeForm FnAppend . formState . (^. form) $ state))
+            . (mode .~ FnAppend)
+            $ state
+        _ -> continue
+            =<< handleEventLensed state form handleFormEvent (KEnterEvent [])
 appEvent state event =
     continue =<< handleEventLensed state form handleFormEvent event
-        
 
 app :: App (State e) e Name
 app = App
@@ -223,8 +170,8 @@ app = App
 
 initialState :: State e
 initialState = State
-   { _mode = Mode1
-   , _form = makeForm Mode1 initialInput
-   , _prompt = makePrompt Mode1 initialInput
+   { _mode = Home
+   , _form = makeModeForm Home initialInput
+   , _output = "<prompt>"
    }
-   where initialInput = Input "m1" "m2.1" "m2.2" "m3" NavCurr
+   where initialInput = Input "" "" "" "" "" ()
