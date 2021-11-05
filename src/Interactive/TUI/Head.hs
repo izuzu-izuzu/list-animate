4a60cc3f010da055a7a14fcc402a2ec9d6cbd31e
izuzu <izuzu@izuzu.local>
2021-11-06 07:22:04 +0800

    feat: Update `head` page layout and widgets

--- src/Interactive/TUI/Head.hs
+++ src/Interactive/TUI/Head.hs
@@ -28,14 +28,30 @@ import Animations.Head
 import Interactive.TUI.Core
 import Interactive.TUI.Home
 import Interactive.TUI.Interpreter
+import Brick.Widgets.Center (hCenter)
+
+makeTitle :: String
+makeTitle = "head :: [a] -> a"
+
+funcDef :: String
+funcDef = "head xs"
 
 makeForm :: Input -> Form Input e Name
 makeForm =
-    setFormConcat (vBox . intersperse (vLimit 1 $ fill '·'))
+    setFormConcat (vBox . (funcDefWidget :))
     . newForm
-        [ (str "xs: " <+>) @@= editTextField arg1 Arg1Field (Just 3)
-        , makeNavField "head"
+        [ (str "xs: " <+>) @@= editTextField arg1 Arg1Field (Just 2)
+        , makeNavField
         ]
+    where funcDefWidget = hCenter $ str funcDef
+
+makeNote :: Widget n
+makeNote = strWrap $ printf
+    "Ensure the list xs contains between 1 and %v elements, and that each \
+    \element can be shown in no more than %v characters.\n\
+    \(Note that Strings are shown with quotation marks.)"
+    maxListLength
+    maxElementLength
 
 loadValidateXs :: MonadIO m => State e -> m (Either InterpreterError String)
 loadValidateXs state = do
@@ -45,10 +61,8 @@ loadValidateXs state = do
 
 loadResult :: MonadIO m => State e -> m (Either InterpreterError String)
 loadResult state = do
-    let
-        Input{_arg1 = xs} = formState $ state ^. form
-        xs' = parens $ unpack xs
-    runLimitedEvalWithType $ printf "head %v" xs'
+    xs' <- loadValidateXs state
+    either (pure . Left) (runLimitedEvalWithType . printf "head %v") xs'
 
 previewEvent :: State e -> EventM Name (Next (State e))
 previewEvent state = do
@@ -57,31 +71,25 @@ previewEvent state = do
     let
         focus = focusGetCurrent . formFocus . (^. form) $ state
         xsStr = either makeErrorMessage id xs
-        animateResultMessage = case (xs, result) of
-            (Right _, Right _) ->
-                "\n\n\
-                \Select [Animate] to view the animation."
-            (Right _, Left _) ->
-                "\n\n\
-                \Something went wrong when preparing the animation.\n\
-                \Ensure your arguments have the correct types, then try again."
-            _ -> "" :: String
-        previewResultMessage = case (xs, result) of
-            (Right _, Right _) ->
-                "\n\n\
-                \Select [Animate] to view the animation."
-            _ -> "" :: String
+        resultStr = either makeErrorMessage id result
+        animateResultPrompt =
+            withAttr "bold" $ str (funcDef <> ": ") <+> strWrap resultStr
+        animatePrompt = case (xs, result) of
+            (Right _, Right _) -> animateResultPrompt
+            (Right _, Left _) -> animateErrorPrompt
+            _ -> emptyWidget
+        previewPrompt = case (xs, result) of
+            (Right _, Right _) -> animateAvailablePrompt
+            _ -> emptyWidget
         prompt = case focus of
-            Just Arg1Field -> "xs: " <> xsStr
-            Just NavPreviewField -> printf
-                "xs: %v%v"
-                xsStr
-                previewResultMessage
-            Just NavAnimateField -> printf
-                "xs: %v%v"
-                xsStr
-                animateResultMessage
-            _ -> ""
+            Just Arg1Field -> str "xs: " <+> strWrap xsStr
+            Just NavPreviewField ->
+                (str "xs: " <+> strWrap xsStr)
+                <=> previewPrompt
+            Just NavAnimateField ->
+                (str "xs: " <+> strWrap xsStr)
+                <=> animatePrompt
+            _ -> emptyWidget
     continue . (output .~ prompt) $ state
 
 animateEvent :: State e -> EventM Name (Next (State e))
@@ -89,7 +97,17 @@ animateEvent state = do
     xs <- either (pure . Left) splitListStr =<< loadValidateXs state
     result <- loadResult state
     case (xs, result) of
-        (Right xs', Right _) -> do
+        (Right xs', Right _) ->
             liftIO . reanimate $ headDynamicAnimation xs'
-            homeEvent state
-        _ -> previewEvent state
+        _ -> pure ()
+    previewEvent state
+
+animateAvailablePrompt :: Widget Name
+animateAvailablePrompt = withAttr "actionAvailable"
+    $ strWrap "Select [Animate] to view the animation."
+
+animateErrorPrompt :: Widget Name
+animateErrorPrompt = withAttr "error"
+    $ strWrap
+        "Something went wrong when preparing the animation.\n\
+        \Ensure your argument has the correct type, then try again."
