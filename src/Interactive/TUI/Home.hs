eb82fa193e0769c98b5df2d80678954eb8e7aeb5
izuzu <izuzu@izuzu.local>
2021-11-06 09:02:11 +0800

    feat: Update home page layout and widgets

--- src/Interactive/TUI/Home.hs
+++ src/Interactive/TUI/Home.hs
@@ -12,22 +12,35 @@ import Brick.Forms
 
 import Interactive.TUI.Core
 
+makeTitle :: String
+makeTitle = "Home"
+
 makeForm :: Input -> Form Input e Name
-makeForm =
-    setFormConcat (vBox . intersperse (vLimit 1 $ fill '·'))
-    . newForm
-        [ radioField
-            emptyInputField
-            [ ((), SelectFnAppendField, "(++) :: [a] -> [a] -> [a]")
-            , ((), SelectFnHeadField, "head :: [a] -> a")
-            , ((), SelectFnTailField, "tail :: [a] -> [a]")
-            ]
+makeForm = newForm
+    [ radioCustomField
+        ' '
+        ' '
+        '•'
+        emptyInputField
+        [ ((), SelectFnAppendField, "(++) :: [a] -> [a] -> [a]" <> rPad)
+        , ((), SelectFnHeadField, "head :: [a] -> a" <> rPad)
+        , ((), SelectFnTailField, "tail :: [a] -> [a]" <> rPad)
         ]
+    ]
+    where rPad = "  "
+
+makeNote :: Widget n
+makeNote = strWrap
+    "Use the arrow keys, Tab and Shift-Tab, or mouse to navigate the menu.\n\
+    \Press Enter to select. Press Esc to quit the app.\n\
+    \\n\
+    \Select a function to continue."
 
 homeEvent :: State e -> EventM Name (Next (State e))
 homeEvent state =
     continue
-    . (output .~ "<prompt>")
+    . (output .~ emptyWidget)
+    . (note .~ makeNote)
     . (form .~ (makeForm . formState . (^. form) $ state))
     . (mode .~ Home)
     $ state
