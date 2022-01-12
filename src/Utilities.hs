56de7b60adaf4685b83d5f9ade42792b45170b4f
izuzu <izuzu@izuzu.local>
2022-01-13 00:12:35 +0800

    feat: Add module for re-exporting all functions

---
+++ src/Utilities.hs
@@ -0,0 +1,23 @@
+{-# OPTIONS_GHC -Wall #-}
+
+{-|
+    Various utility functions.
+-}
+module Utilities
+    ( module Utilities.Easing
+    , module Utilities.Effect
+    , module Utilities.LaTeX
+    , module Utilities.ListTemplates
+    , module Utilities.Misc
+    , module Utilities.Scene
+    , module Utilities.Svg
+    )
+    where
+
+import Utilities.Easing
+import Utilities.Effect
+import Utilities.LaTeX
+import Utilities.ListTemplates
+import Utilities.Misc
+import Utilities.Scene
+import Utilities.Svg
